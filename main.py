64d8822322a42330eb6edc123d273ad554d4f6dc
Edenskull <maxchamp@live.fr>
2019-03-04 19:39:52 +0100

    Code Breath

--- main.py
+++ main.py
@@ -3,85 +3,8 @@ import time
 import random
 import sys
 
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from plugins.utils import print_same_line
 from plugins.config import CONFIGURATION
-
-
-class InstagramBot:
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-        self.driver = webdriver.Firefox()
-
-    def close_browser(self):
-        self.driver.close()
-
-    def login(self):
-        driver = self.driver
-        driver.get("https://www.instagram.com/")
-        time.sleep(2)
-        login_button = driver.find_element_by_xpath("//a[@href='/accounts/login/?source=auth_switcher']")
-        login_button.click()
-        time.sleep(2)
-        user_name_elem = driver.find_element_by_xpath("//input[@name='username']")
-        user_name_elem.clear()
-        user_name_elem.send_keys(self.username)
-        passworword_elem = driver.find_element_by_xpath("//input[@name='password']")
-        passworword_elem.clear()
-        passworword_elem.send_keys(self.password)
-        passworword_elem.send_keys(Keys.RETURN)
-        time.sleep(2)
-
-    def like_photo(self, hashtag):
-        driver = self.driver
-        driver.get("https://www.instagram.com/explore/tags/" + hashtag + "/")
-        time.sleep(2)
-
-        # gathering photos
-        pic_hrefs = []
-        for i in range(1, 7):
-            try:
-                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-                time.sleep(2)
-                # get tags
-                hrefs_in_view = driver.find_elements_by_tag_name('a')
-                # finding relevant hrefs
-                hrefs_in_view = [
-                    elem.get_attribute('href') for elem in hrefs_in_view
-                    if '.com/p/' in elem.get_attribute('href')
-                ]
-                # building list of unique photos
-                [pic_hrefs.append(href) for href in hrefs_in_view if href not in pic_hrefs]
-                # print("Check: pic href length " + str(len(pic_hrefs)))
-            except Exception:
-                continue
-
-        # Liking photos
-        unique_photos = len(pic_hrefs)
-        for pic_href in pic_hrefs:
-            driver.get(pic_href)
-            time.sleep(2)
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            try:
-                time.sleep(random.randint(2, 4))
-                like_button = lambda: driver.find_element_by_xpath('//span[@aria-label="J’aime"]')
-                like_button().click()
-                time.sleep(1)
-                sub_button = lambda: driver.find_element_by_xpath("//div/button[text()='S’abonner']")
-                sub_button().click()
-                pseudo_insta = driver.find_element_by_xpath("//h2/a")
-                print(pseudo_insta.text)
-                for second in reversed(range(0, random.randint(18, 28))):
-                    print_same_line(
-                        "#" + hashtag + ': unique photos left: ' + str(unique_photos)
-                        + " | Sleeping " + str(second)
-                    )
-                    time.sleep(1)
-            except Exception:
-                time.sleep(2)
-            unique_photos -= 1
+from plugins.bot import InstagramBot
 
 
 if __name__ == "__main__":
